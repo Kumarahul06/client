474dbe5ad6a9f2fb3edd376ccdcdea25b960aa74
Kumarahul06 <kumarahul1321@gmail.com>
2023-07-15 12:35:15 +0530

    new work

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+ import Canvas from './canvas';
+ import Customizer from './pages/Customizer';
+import home from './pages/home';
+ 
+ 
+ 
+ function App() {
+return (
+   <div>
+    <h1 className="head-text">ThreeJS</h1>
+   </div>
+  )
+}
+
+export default App
+  
